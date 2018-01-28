1a8a91de677ae1c8151d6a6206c6afd9333e4638
Jack Moran <jack@earth.co.nz>
2018-01-28 14:54:07 +1300

    Fetch initial store data from a server

--- src/index.tsx
+++ src/index.tsx
@@ -8,32 +8,17 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import leaderboardApp from './reducers';
 
-const data = {
-  regions: {
-    '04jp51zq': { id: '04jp51zq', name: 'Wellington' },
-    'ltpkbcoe': { id: 'ltpkbcoe', name: 'Auckland' },
-    'cmh2h4r8': { id: 'cmh2h4r8', name: 'Napier' },
-    'ahqq2j94': { id: 'ahqq2j94', name: 'London' },
-    'txlcs06u': { id: 'txlcs06u', name: 'World' }
-  },
-  users: {
-    'gb9mi5xl': { id: 'gb9mi5xl', name: 'Person 0' },
-    'z3g4ccgj': { id: 'z3g4ccgj', name: 'Person 1' },
-    'w9xacb5s': { id: 'w9xacb5s', name: 'Person 2' },
-    'p0lxe44x': { id: 'p0lxe44x', name: 'Person 3' },
-    'e14m9b5u': { id: 'e14m9b5u', name: 'Person 4' }
-  },
-  results: [
-    { region: 'ltpkbcoe', winner: 'gb9mi5xl', date: '2018-01-27', score: 0},
-    { region: 'txlcs06u', winner: 'p0lxe44x', date: '2018-01-23', score: 45}
-  ]
-};
-const store = createStore(leaderboardApp, data);
+fetch('/api/state', { accept: 'application/json' } as RequestInit).then((res) => {
+  return res.json();
+}).then((data) => {
+  const store = createStore(leaderboardApp, data);
 
-ReactDOM.render(
-  <Provider store={store}>
+  ReactDOM.render(
+    <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+    </Provider>,
+    document.getElementById('root') as HTMLElement
+  );
+
+  registerServiceWorker();
+});
