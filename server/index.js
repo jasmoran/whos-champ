5f2924e2287394964135ea75fae2e9a3d3726ba8
Jack Moran <jack@earth.co.nz>
2018-02-26 22:25:40 +1300

    Store creator and creation time with new records Extract read and create functionality Pick which fields to save to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const axios = require('axios');
 
 const express = require('express')
 const PORT = process.env.PORT || 3001
@@ -37,40 +38,75 @@ app.use('/api', checkJwt);
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
+// User cache
+var userCache = {};
+
 (async function() {
   const client = await mongo.MongoClient.connect(uri);
   const db = client.db(dbName);
   console.log('Connected to MongoDB');
 
   const Regions = db.collection('regions');
-  const Users = db.collection('users');
+  const Players = db.collection('players');
   const Results = db.collection('results');
+  const Users = db.collection('users');
 
-  app.get('/api/state', async function (req, res) {
-    let data = {};
-    let p_reg = Regions.find().toArray();
-    let p_usr = Users.find().toArray();
-    let p_res = Results.find().toArray();
-    [data.regions, data.users, data.results] = await Promise.all([p_reg, p_usr, p_res]);
+  const exclude = { _id: 0, location: 0, createdAt: 0, creator: 0 };
 
-    res.send(data);
-  });
+  // Fetch user data
+  async function fetchUser(sub, auth) {
+    if (userCache[sub]) {
+      return userCache[sub];
+    } else {
+      try {
+        const usrreq = await axios.get('https://app86758601.auth0.com/userinfo',
+          { headers: { 'Authorization': auth } });
+        const data = usrreq.data;
+        Users.updateOne({ sub }, { $set: data }, { upsert: true });
+        userCache[sub] = data;
+        return data;
+      } catch(err) {
+        console.error(err);
+        return { sub };
+      }
+    }
+  }
 
-  app.get('/api/regions', async function (req, res) {
-    res.send(await Regions.find().toArray());
-  });
+  function read(collection) {
+    return async (req, res) => {
+      res.send(await collection.find().project(exclude).toArray());
+    };
+  }
 
-  app.get('/api/players', async function (req, res) {
-    res.send(await Users.find().toArray());
-  });
+  function create(collection, pick) {
+    return async (req, res) => {
+      const user = await fetchUser(req.user.sub, req.headers.authorization);
+      const body = req.body;
 
-  app.get('/api/results', async function (req, res) {
-    res.send(await Results.find().toArray());
-  });
+      collection.insert({
+        ...pick(body),
+        id: body.id,
+        location: body.location,
+        createdAt: new Date(),
+        creator: {
+          email: user.email,
+          sub: user.sub
+        }
+      });
 
-  app.post('/api/results', function (req, res) {
-    Results.insert(req.body);
-  });
+      res.sendStatus(201);
+    };
+  }
+
+
+  app.get('/api/regions', read(Regions));
+  app.post('/api/regions', create(Regions, ({name}) => ({name})));
+
+  app.get('/api/players', read(Players));
+  app.post('/api/players', create(Players, ({name}) => ({name})));
+
+  app.get('/api/results', read(Results));
+  app.post('/api/results', create(Results, ({regions, winner, date, score}) => ({regions, winner, date, score})));
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
