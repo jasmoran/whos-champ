d80ea63d842c3a027d29eafe800d8c4e3efe8152
Jack Moran <jack@earth.co.nz>
2018-04-22 21:15:26 +1200

    Set up client side GraphQL using React Apollo

--- react-ui/src/index.tsx
+++ react-ui/src/index.tsx
@@ -9,11 +9,28 @@ import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import leaderboardApp from './reducers';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({ uri: '/graphql' });
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 const store = createStore(
   leaderboardApp,
   applyMiddleware(thunkMiddleware)
 );
 const root = document.getElementById('root') as HTMLElement;
-ReactDOM.render(<Provider store={store}><App /></Provider>, root);
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
+  root);
 
 registerServiceWorker();
